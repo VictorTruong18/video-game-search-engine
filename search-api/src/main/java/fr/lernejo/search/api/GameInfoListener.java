c2d8fedbeba6b38d3dfc870f0c88068c11b25379
VictorTruong18 <victor.truong18@gmail.com>
2022-02-05 18:03:58 +0100

    Add rabbitmq listener GameInfoListener class
    We add messages inside the queue, we have the method onMessage wh-
    ich will listen to every incoming messages thanks to @RabbitListe-
    ner. The checkpoint is validated on this commit.

---
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -0,0 +1,29 @@
+package fr.lernejo.search.api;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GameInfoListener {
+    private final RestHighLevelClient client;
+
+    public GameInfoListener(RestHighLevelClient restHighLevelClient) {
+        this.client = restHighLevelClient;
+    }
+
+    @RabbitListener(queues =  AmqpConfiguration.GAME_INFO_QUEUE)
+    public void onMessage(String msg, @Header("game_id") String id) throws IOException {
+        IndexRequest indexRequest = new IndexRequest("games");
+        indexRequest.id(id);
+        indexRequest.source(msg, XContentType.JSON);
+        System.out.println("Message received " + msg + "\n");
+        this.client.index(indexRequest, RequestOptions.DEFAULT);
+    }
+}
