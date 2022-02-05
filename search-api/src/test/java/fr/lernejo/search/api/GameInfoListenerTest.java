118e61541b58bf3116cbe2efbb7eff51a2c3c26c
VictorTruong18 <victor.truong18@gmail.com>
2022-02-05 22:13:40 +0100

    Update pom files for test coverage
    My tests were not running when I was doing mvn test. I hope this w-
    ill fix it ! code coverage : 13%.

--- search-api/src/test/java/fr/lernejo/search/api/GameInfoListenerTest.java
+++ search-api/src/test/java/fr/lernejo/search/api/GameInfoListenerTest.java
@@ -1,22 +1,11 @@
 package fr.lernejo.search.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.lernejo.search.api.GameInfoListener;
-import fr.lernejo.search.api.Launcher;
-import org.springframework.amqp.core.Message;
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.core.MessageBuilder;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-
-public class GameInfoListenerTest {
+class GameInfoListenerTest {
 
     @Test
     void test_listener_sucess(){
@@ -33,7 +22,6 @@ public class GameInfoListenerTest {
     void test_listener_missing_id(){
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
             RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
-            rabbitTemplate.setRoutingKey("game_info");
             rabbitTemplate.convertAndSend("", "game_info", "{'test' : 'working' }", m -> {
                 return m;
             });
