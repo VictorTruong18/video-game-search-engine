b721ac62777d0a3e1004a892e130245939aa8cf7
VictorTruong18 <victor.truong18@gmail.com>
2022-02-05 19:31:35 +0100

    Add tests for GameInfo Listener
    We are testing when the message is rightly written and when it's m
    issing the Id. Also added the condition if the id is missing. Cod-
    e coverage of previous commit : 71%.

---
+++ search-api/src/test/java/fr/lernejo/search/api/tests/GameInfoListenerTest.java
@@ -0,0 +1,42 @@
+package fr.lernejo.search.api.tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.lernejo.search.api.GameInfoListener;
+import fr.lernejo.search.api.Launcher;
+import org.springframework.amqp.core.Message;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+public class GameInfoListenerTest {
+
+    @Test
+    void test_listener_sucess(){
+        try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            rabbitTemplate.convertAndSend("", "game_info", "{'test' : 'working' }", m -> {
+                m.getMessageProperties().getHeaders().put("game_id", "1");
+                return m;
+            });
+        }
+    }
+
+    @Test
+    void test_listener_missing_id(){
+        try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            rabbitTemplate.setRoutingKey("game_info");
+            rabbitTemplate.convertAndSend("", "game_info", "{'test' : 'working' }", m -> {
+                return m;
+            });
+        }
+    }
+}
