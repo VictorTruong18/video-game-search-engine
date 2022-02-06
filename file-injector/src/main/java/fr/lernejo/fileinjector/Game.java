3b2aed147b11bf4e03802744f2a7540b51d1b0af
VictorTruong18 <victor.truong18@gmail.com>
2022-02-06 09:12:52 +0100

    Add FileInjector
    The logic of the FileInjector will take the absolute path of the f-
    ile as a main argument. All the file will be send to the game-info
    queue. Current coverage : 93%

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Game.java
@@ -0,0 +1,15 @@
+package fr.lernejo.fileinjector;
+
+public class Game {
+    public Number id;
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+    public String game_url;
+    public String freetogame_profile_url;
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,34 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class Launcher {
-
     public static void main(String[] args) {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
+            File file = Paths.get(args[0]).toFile();
+            if (!file.exists()) throw new FileNotFoundException();
+            Game[] games = new ObjectMapper().readValue(file, Game[].class);
+            RabbitTemplate template = springContext.getBean(RabbitTemplate.class);
+            for (Game game : games) {
+                template.setMessageConverter(new Jackson2JsonMessageConverter());
+                template.convertAndSend("", "game_info", game, m -> {
+                    m.getMessageProperties().setHeader("game_id", game.id);
+                    return m;
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
