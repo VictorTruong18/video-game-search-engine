861fab60a6af4bcf902055573d5c14f486bbc588
VictorTruong18 <victor.truong18@gmail.com>
2022-02-06 09:57:14 +0100

    Add test to FileInjector
    Simple test that tests the good behaving of the main function.
    Current coverage : 53%.

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -1,5 +1,17 @@
 package fr.lernejo.fileinjector;
 
-class LauncherTest {
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+class LauncherTest {
+    @Test
+    void test_good_file_inject_success() {
+        File resourcesDirectory = new File("src/test/resources");
+        String file = resourcesDirectory.getAbsolutePath()+"/games.json";
+        assertTimeoutPreemptively(Duration.ofSeconds(60L),() -> Launcher.main(new String[]{file}));
+    }
 }
